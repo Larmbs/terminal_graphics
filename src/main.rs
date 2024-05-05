e1998a6cff28f70b4314dd1b4b3cd0763260613c
Liam <liamisrael200@gmail.com>
2024-05-05 14:31:53 -0400

    h

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,25 @@
 use screen::PixelScreen;
-
+use std::thread;
+use std::time::Duration;
 mod screen;
 
 fn main() {
-    let mut screen = screen::Canvas::new(80, 30, 250);
+    screen::init();
+    let mut screen = screen::Canvas::new(40, 30, 250);
     screen.draw_line(0, 0, 50, 20, 0);
     screen.draw_circle(10, 10, 10, 100);
     screen.display();
     let mut x = 0;
+
     loop {
-        x += 1;
-        screen.draw_line(x, 0, 50 + x, 20, 0);
+        screen.fill(250);
+        x += 2;
+
+        x %= 41;
+
+        screen.draw_line(x, 30, 0, 0, 0);
+        screen.draw_circle(x/2, 15, 7, 60);
         screen.display();
+        thread::sleep(Duration::from_millis(30));
     }
 }
--- src/screen/mod.rs
+++ src/screen/mod.rs
@@ -5,13 +5,30 @@ mod canvas;
 pub use screen::Screen;
 pub use canvas::Canvas;
 
+use std::io;
+
+use crossterm::{execute, terminal::{Clear, ClearType}, cursor::MoveTo};
+
+fn clear_screen() -> io::Result<()> {
+    execute!(io::stdout(), Clear(ClearType::All))?;
+    Ok(())
+}
+
+fn set_cursor_to_start() -> io::Result<()> {
+    execute!(io::stdout(), MoveTo(0, 0))?;
+    Ok(())
+}
+
+pub fn init() {
+    clear_screen().unwrap();
+}
 
 pub trait PixelScreen {
     fn get_size(&self) -> (u8, u8);
     fn set_pixel(&mut self, x: usize, y: usize, color: u8);
     fn fill(&mut self, color: u8);
     fn clear_screen(&self) {
-        print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+        set_cursor_to_start().unwrap();
     }
     fn display(&self);
 }
